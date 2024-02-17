b2cd691dedd0d1456efa13b41808bfae7d5bb34b
seu-username <seu-email>
2024-02-17 13:17:01 -0300

    Primeiro Commit - Projeto Mario

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const form = document.querySelector (".formulario-fale-conosco")
+const mascara = document.querySelector (".mascara-formulario")
+
+function mostrarform() {
+    form.style.left = "50%"
+    form.style.top = "33%"
+  mascara.style.visibility = "visible"
+      
+}
+
+function esconderform() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+
+}
